4de09790b018e619f500377e14c6313d9cd3a484
Devshe <sheroniperera20@gmail.com>
2023-09-15 17:22:22 +0530

    Feat: added budget alerts

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo } from 'react';
+import { useCallback, useEffect, useMemo } from 'react';
 import { Pressable, ScrollView, View } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import { Text } from 'react-native-paper';
@@ -8,6 +8,7 @@ import { BalanceDialog, Button, Divider, ExpenseDialog } from '@/components';
 import { colors } from '@/config/theme';
 import { MoveRight } from '@/icons';
 import { store } from '@/store';
+import { updateBudget } from '@/store/reducers/data/budgets';
 import { currentMonthExpenses, setSelectedExpense } from '@/store/reducers/data/expenses';
 import { toggleBalanceDialog, toggleExpenseDialog } from '@/store/reducers/ui/home';
 import styles from './styles';
@@ -26,11 +27,23 @@ const onBalancePress = () => store.dispatch(toggleBalanceDialog(true));
 const Home = () => {
   const expenses = useSelector(currentMonthExpenses);
   const startingBalance = useSelector((state) => state.data.expenses.startingBalance);
+  const budgets = useSelector((state) => state.data.budgets.all);
 
   const savings = useMemo(() => {
     if (startingBalance === null) return 0;
     return expenses.reduce((acc, expense) => acc - expense.amount, startingBalance);
   }, [expenses, startingBalance]);
+
+  useEffect(() => {
+    const totalExpenses = startingBalance - savings;
+    for (const budget of budgets) {
+      if (totalExpenses > budget.amount && !budget.warned) {
+        store.dispatch(updateBudget({ ...budget, warned: true }));
+        global.alert.warn(`You have exceeded a budget of LKR ${budget.amount?.toFixed(2)}!`);
+        break;
+      }
+    }
+  }, [startingBalance, savings]);
 
   const renderItem = useCallback(({ item: expense }) => (
     <Pressable key={expense.day} style={styles.expenseCard(expense.day)} onPress={() => onExpenseClick(expense)}>
